790fb2d09c912299f6c2956bfb2623adf2c452e5
revathimsc <103559154+revathimsc@users.noreply.github.com>
2022-07-26 16:46:11 +0530

    Add files via upload

---
+++ arrow & array/app.js
@@ -0,0 +1,5 @@
+let greet = (user)=> {
+    console.log("hello" + user);
+    return 1;
+}
+console.log(greet('revathi'));
---
+++ arrow & array/array.js
@@ -0,0 +1,14 @@
+var a1=["p","q","r","s"];
+var a2=["a","b"];
+//push
+console.log(a1.push("t"));
+console.log(a1);
+//pop
+console.log(a1.pop());
+console.log(a1);
+//concat
+console.log(a1.concat(a1));
+//join
+console.log(a1.join("*"));
+//reverse
+console.log(a1.reverse());
